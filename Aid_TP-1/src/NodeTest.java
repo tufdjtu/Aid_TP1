3656b54f538ed7f8eeb7504d8dd763d94d775045
Francisco Vicente <82675535+tufdjtu@users.noreply.github.com>
2022-05-17 14:43:00 +0100

    Add files via upload

---
+++ Aid_TP-1/src/NodeTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NodeTest {
+
+	@Test
+	void testTroca() {
+		String charArray = get
+	}
+
+	@Test
+	void testOrdenarPuzzle() {
+		fail("Not yet implemented");
+	}
+
+}
